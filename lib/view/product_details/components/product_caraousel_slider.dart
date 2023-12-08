6ab0bee7afa37a8b6ad5d58b8ff6c8119841caeb
Vyankatesh Kshatriya <vyankatesh.kshatriya@gmail.com>
2023-12-08 15:47:24 +0530

    update

---
+++ lib/view/product_details/components/product_caraousel_slider.dart
@@ -0,0 +1,101 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:e_commerce_app/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+
+class ProductCarouselSlider extends StatefulWidget {
+  const ProductCarouselSlider({super.key, required this.images});
+
+  final List<String> images;
+
+  @override
+  State<ProductCarouselSlider> createState() => _ProductCarouselSliderState();
+}
+
+class _ProductCarouselSliderState extends State<ProductCarouselSlider> {
+  // ignore: unused_field
+  int _currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Stack(
+          children: [
+            CarouselSlider(
+              items: widget.images
+                  .map(
+                    (e) => Container(
+                      color: Colors.white,
+                      child: CachedNetworkImage(
+                        imageUrl: baseUrl + e,
+                        imageBuilder: (context, imageProvider) => Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            image: DecorationImage(
+                              image: imageProvider,
+                              fit: BoxFit.contain,
+                            ),
+                          ),
+                        ),
+                        placeholder: (context, url) => Shimmer.fromColors(
+                          baseColor: Colors.grey.shade300,
+                          highlightColor: Colors.white,
+                          child: Container(
+                            color: Colors.grey.shade300,
+                          ),
+                        ),
+                        errorWidget: (context, url, error) => const Center(
+                          child: Icon(
+                            Icons.error_outline,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                  .toList(),
+              options: CarouselOptions(
+                aspectRatio: 1.5,
+                viewportFraction: 1,
+                onPageChanged: (index, reason) {
+                  setState(() {
+                    _currentIndex = index;
+                  });
+                },
+              ),
+            ),
+            Positioned(
+              child: IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: Icon(
+                  Icons.arrow_back,
+                  color: Theme.of(context).primaryColor,
+                ),
+              ),
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: widget.images.map((e) {
+            int index = widget.images.indexOf(e);
+            return Container(
+              width: 6,
+              height: 6,
+              margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 3),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: _currentIndex == index
+                    ? Colors.grey.shade600
+                    : Colors.grey.shade400,
+              ),
+            );
+          }).toList(),
+        ),
+      ],
+    );
+  }
+}
--- lib/view/product_details/product_details_screen.dart
+++ lib/view/product_details/product_details_screen.dart
@@ -1,14 +1,194 @@
+import 'package:e_commerce_app/view/product_details/components/product_caraousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:e_commerce_app/model/popular_product.dart';
+import 'package:intl/intl.dart';
 
-class ProductDetailScreen extends StatelessWidget {
+class ProductDetailScreen extends StatefulWidget {
   const ProductDetailScreen({super.key, required this.product});
 
   final Product product;
+
+  @override
+  State<ProductDetailScreen> createState() => _ProductDetailScreenState();
+}
+
+class _ProductDetailScreenState extends State<ProductDetailScreen> {
+  NumberFormat formatter = NumberFormat('00');
+  int _qty = 1;
+  int _tagIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(children: []),
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ProductCarouselSlider(images: widget.product.images),
+            const SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Text(
+                widget.product.name,
+                style: TextStyle(
+                  fontSize: 28,
+                  color: Theme.of(context).primaryColor,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Text(
+                'Rs. ${widget.product.tags.first.price.toStringAsFixed(2)}',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Theme.of(context).primaryColor,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 1),
+                      borderRadius: const BorderRadius.all(Radius.circular(8)),
+                    ),
+                    child: Row(
+                      children: [
+                        InkWell(
+                          onTap: () {
+                            if (_qty > 1) {
+                              setState(() {
+                                _qty--;
+                              });
+                            }
+                          },
+                          child: Icon(
+                            Icons.keyboard_arrow_left_sharp,
+                            size: 32,
+                            color: Colors.grey.shade800,
+                          ),
+                        ),
+                        Text(
+                          formatter.format(_qty),
+                          style: TextStyle(
+                            color: Colors.grey.shade800,
+                            fontSize: 18,
+                          ),
+                        ),
+                        InkWell(
+                          onTap: () {
+                            setState(() {
+                              _qty++;
+                            });
+                          },
+                          child: Icon(
+                            Icons.keyboard_arrow_right_sharp,
+                            size: 32,
+                            color: Colors.grey.shade800,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 1),
+                      borderRadius: const BorderRadius.all(Radius.circular(8)),
+                    ),
+                    child: Row(
+                      children: [
+                        InkWell(
+                          onTap: () {
+                            if (_tagIndex > 0) {
+                              setState(() {
+                                _tagIndex--;
+                              });
+                            }
+                          },
+                          child: Icon(
+                            Icons.keyboard_arrow_left_sharp,
+                            size: 32,
+                            color: Colors.grey.shade800,
+                          ),
+                        ),
+                        Text(
+                          widget.product.tags[_tagIndex].title,
+                          style: TextStyle(
+                            color: Colors.grey.shade800,
+                            fontSize: 18,
+                          ),
+                        ),
+                        InkWell(
+                          onTap: () {
+                            if (_tagIndex != widget.product.tags.length - 1) {
+                              setState(() {
+                                _tagIndex++;
+                              });
+                            }
+                          },
+                          child: Icon(
+                            Icons.keyboard_arrow_right_sharp,
+                            size: 32,
+                            color: Colors.grey.shade800,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Text(
+                'About this product:',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Theme.of(context).primaryColor,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Text(
+                widget.product.description,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey.shade600,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: TextButton(
+          onPressed: () {},
+          style: ButtonStyle(
+            foregroundColor: const MaterialStatePropertyAll(Colors.white),
+            backgroundColor:
+                MaterialStatePropertyAll(Theme.of(context).primaryColor),
+          ),
+          child: const Text('Add to Cart'),
+        ),
+      ),
     );
   }
 }
